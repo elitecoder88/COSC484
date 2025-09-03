b240676ac1562d0a68fbbed0db00ef1bc3f78826
elitecoder88 <160454217+elitecoder88@users.noreply.github.com>
2025-09-02 21:18:41 -0400

    Assignment8 Completed

---
+++ Assignment8/Assignment8/profiler/src/App.js
@@ -0,0 +1,13 @@
+import Profile from './index.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Profile />
+      </header>
+    </div>
+  );
+}
+
+export default App;
